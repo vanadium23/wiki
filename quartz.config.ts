25e90fc1b6256edfb43b7293ffec27adcc8f7a48
vanadium23 <chernoffivan@gmail.com>
2024-11-29 20:01:36 +0300

    config: remove spa routes

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "ChernoWiki",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: null,
     locale: "ru-RU",
