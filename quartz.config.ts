9c7c9a360cc751d117b2a980652d2ce88dcb77dc
vanadium23 <chernoffivan@gmail.com>
2024-12-03 16:02:18 +0300

    styles: merge background highlight for internal links

--- quartz.config.ts
+++ quartz.config.ts
@@ -35,7 +35,7 @@ const config: QuartzConfig = {
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(54, 54, 54, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
