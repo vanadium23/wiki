196681dc82cdfbe0ddcb3e802409e013e1d1750a
vanadium23 <chernoffivan@gmail.com>
2025-03-26 11:45:26 +0300

    quartz: remake dates in ContentMeta component

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -19,6 +19,27 @@ const defaultOptions: ContentMetaOptions = {
   showComma: true,
 }
 
+function toIsoDate(d: Date): String {
+  return d.toISOString().substring(0, 10)
+}
+
+function diffDates(d1: Date, d2: Date): String {
+  const ms = Math.abs(d2 - d1)
+  const hours = Math.floor(ms / 1000 / 60 / 60)
+  if (hours < 24) return `${hours}h`
+  const days = Math.floor(hours / 24)
+  if (days < 7) return `${days}d`
+
+  const weeks = Math.floor(days / 7)
+  if (weeks < 4) return `${weeks}w`
+
+  const months = Math.floor(days / 30.44)
+  if (months < 12) return `${months}m`
+
+  const years = Math.floor(days / 365.25)
+  return `${years}y`
+}
+
 export default ((opts?: Partial<ContentMetaOptions>) => {
   // Merge options with defaults
   const options: ContentMetaOptions = { ...defaultOptions, ...opts }
@@ -30,16 +51,13 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       const segments: (string | JSX.Element)[] = []
 
       if (fileData.dates) {
-        if (fileData.dates.created) {
-          segments.push(
-            <span>Создано: {formatDate(fileData.dates.created, cfg.locale)}</span>
-          )
-        }
-        if (fileData.dates.modified) {
-          segments.push(
-            <span>Обновлено: {formatDate(fileData.dates.modified, cfg.locale)}</span>
-          )
-        }
+        const age = diffDates(fileData.dates.created, fileData.dates.modified)
+        segments.push(
+          <span>
+            {toIsoDate(fileData.dates.created)} &mdash; {toIsoDate(fileData.dates.modified)} (~{age}
+            )
+          </span>,
+        )
       }
 
       // Display reading time if enabled
